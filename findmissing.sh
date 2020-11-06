72cc7fe4ccaef5f0ab4c452d7c2a622dda737d6c
Kurausukun <lord.uber1@gmail.com>
2020-11-05 21:28:40 -0500

    update samples

--- findmissing.sh
+++
@@ -1,6 +0,0 @@
-top_file=sound/music_player_table.inc  # Replace this with the offending C or ASM file.
-for file in $(tools/scaninc/scaninc -I include ${top_file}); do
-  if ! [ -f "${file}" ]; then
-    make "${file}" || echo ${file}
-  fi
-done
